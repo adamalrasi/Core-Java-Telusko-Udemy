42641b7cb038303a5a2a432df61585d767577be6
Adam Alrasi <adam.alrasi1@gmail.com>
2024-03-17 13:54:44 +0000

    First commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+class Hello {
+    public static void main(String[] args){
+
+        // byte b = 125;
+        int a =  257;
+        byte k = (byte)a;
+
+        float f = 5.6f;
+        int t = (int)f;
+
+        System.out.println(t);
+        
+
+    }
+}
